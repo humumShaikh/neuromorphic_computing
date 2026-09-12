87232d65bfcbb17d8208e7d58fe35f6358657621
CWIR3 <humumshaikh0@gmail.com>
2026-09-12 17:47:39 +0530

    Update spike_encoder.v

--- Digital_SNN/Spike_Encoder/spike_encoder.v
+++ Digital_SNN/Spike_Encoder/spike_encoder.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: CWIR3
+// Engineer: copperwire
 // 
 // Create Date: 09/07/2026 08:07:50 PM
 // Design Name: 
